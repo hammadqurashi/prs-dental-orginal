25737f9889ba99a5913757b11fec43741e4ecb8f
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-16 15:21:33 +0600

    inital commit

---
+++ src/components/global/Header.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Burger from "./Burger";
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+import Topbar from "./Topbar";
+import MobileMenu from "./MobileMenu";
+import { Button } from "@material-tailwind/react";
+
+const Header = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  return (
+    <header>
+      <Topbar />
+      <div className="flex justify-between items-center container mx-auto px-2 py-6 xl:py-0">
+        <Logo />
+
+        <div className="flex items-center">
+          <Navigation />
+          <a
+            href="tel:01133228354"
+            className="mr-2  font-medium text-gray-600 ml-3 block xl:hidden "
+          >
+            <Button
+              variant="outlined"
+              className=" flex items-center text-sm px-1.5 whitespace-nowrap"
+            >
+             0113 322 8354
+            </Button>
+          </a>
+          <a
+            href="tel:01133228354"
+            className="mr-2  font-medium text-gray-600 ml-3 block xl:hidden "
+          >
+            <Button
+              variant="outlined"
+              className=" flex items-center text-sm px-1.5 whitespace-nowrap"
+            >
+             0113 322 8354
+            </Button>
+          </a>
+
+          <Burger open={openDrawer} setOpen={setOpenDrawer} />
+        </div>
+      </div>
+
+      <MobileMenu open={openDrawer} setOpen={setOpenDrawer} />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/services/ClearAligners/ClrarContent.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const ClearAlignersBaner = () => {
+  return (
+    <section className="container mx-auto px-10 py-12">
+      <div className="tab1 py-16">
+        <h1 className="blue text-3xl t-style	pb-16">What are Clear Aligners?</h1>
+        <p className="p-style">
+          Clear Aligners are transparent plastic trays designed to gradually
+          shift your teeth into their ideal positions. Unlike metal braces,
+          these aligners are virtually invisible and can be easily removed for
+          eating and cleaning. They have become increasingly popular due to
+          their convenience, aesthetics, and effectiveness.
+        </p>
+      </div>
+      <div className="tab1 py-16">
+        <h1 className="blue text-3xl t-style 	pb-16">
+          Benefits of Clear Aligners
+        </h1>
+        <div className="flexe">
+        <div className="nove">
+          <h2 className="blue text-2xl	pt-2.5 ">Discreet Appearance</h2>
+          <p>
+            Unlike metal braces, clear aligners are virtually invisible. This
+            makes them an excellent option for adults and teens who want to
+            improve their smile without drawing attention to their treatment.
+          </p>
+        </div>
+        <div className="nove"> 
+          <h2 className="blue text-2xl	pt-2.5 ">Comfort</h2>
+
+          <p>
+            Made of smooth plastic, these aligners are more comfortable than
+            traditional braces, which may have sharp wires or brackets.
+          </p>
+        </div>
+        <div className="nove">
+          <h2 className="blue text-2xl	pt-2.5 ">Convenience</h2>
+
+          <p>
+            Clear aligners can be easily removed, making it much easier to eat,
+            drink, and maintain good oral hygiene.
+          </p>
+        </div>
+        <div className="nove">
+          <h2 className="blue text-2xl	pt-2.5 ">Customization</h2>
+
+          <p>
+            Each set of aligners is custom-made using 3D imaging technology to
+            fit snugly over your teeth and guide them to their ideal position.
+          </p>
+        </div>
+        </div>
+      </div>
+
+      <div className="tab1 py-16">
+        <h1 className="blue text-3xl t-style-2	pb-16">What are Clear Aligners?</h1>
+        <p className="p-style-2">
+          There are several brands offering clear aligners. Here are some
+          popular ones:
+        </p>
+        <p className="p-style-2">• Reveal</p>
+        <p className="p-style-2">• Invisalign</p>
+        <p className="p-style-2">• SmileDirectClub </p>
+        <p className="p-style-2">• Clear Correct</p>
+        <p className="p-style-2"> • AlignerCo</p>
+        <p className="p-style-2">
+          It's important to consult with an orthodontist to determine which type
+          is best suited for your unique needs.
+        </p>
+      </div>
+      <div className="tab1 py-16">
+        <h1 className="blue text-3xl t-style-2	pb-16">Cost of Clear Aligners</h1>
+        <p className="p-style-2">
+          The cost of clear aligners varies depending on various factors such as
+          the complexity of your case, the brand you choose, and your location.
+          However, they are generally priced between £2800 to £4400
+        </p>
+      </div>
+      <div className="tab1 py-16">
+        <h1 className="blue text-3xl t-style-2	pb-16">How to Get Started</h1>
+        <p className="p-style-2">
+          1. Consultation: Visit Parish Dental for a comprehensive evaluation.
+        </p>
+        <p className="p-style-2">
+          2. Treatment Plan: Based on your evaluation, a custom treatment plan
+          will be devised.
+        </p>
+        <p className="p-style-2">
+          3. 3D Imaging: Your teeth will be scanned or molded to create a series
+          of aligners tailored to you.{" "}
+        </p>
+        <p className="p-style-2">
+          4. Start Treatment: Once your aligners are ready, you can start
+          wearing them, typically switching to a new set every two weeks.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ClearAlignersBaner;
