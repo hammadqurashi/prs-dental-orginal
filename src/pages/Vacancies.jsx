67c0360eb3dc7b08603b79bca0f4a83b7481f14f
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-18 01:05:55 +0600

    commit

--- src/pages/Vacancies.jsx
+++ src/pages/Vacancies.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/global.css"
 import Layout from "../components/global/Layout";
 import PageHeader from "../components/global/PageHeader";
 import DynamicSeo from "../components/global/DynamicSeo";
@@ -12,10 +13,23 @@ const Vacancies = () => {
           <div className="card-11">
             <h2>Apprentice Nurse</h2>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
-              repellendus? Iusto debitis voluptatum velit impedit fuga,
-              inventore cum nesciunt officiis unde tempora quasi, assumenda
-              provident praesentium esse natus vero commodi.
+              apprentice nurse works under the supervision of registered nurses
+              and other healthcare professionals to provide basic patient care
+              and support services. This role is designed to provide hands-on
+              experience and training for individuals pursuing a career in
+              nursing.
+            </p>
+            <a href="/contact">Apply Now</a>
+          </div>
+          <div className="card-11">
+            <h2>Registered Nurse</h2>
+            <p>
+              Registered Nurse (RN) is a highly skilled healthcare professional
+              responsible for providing and coordinating patient care in various
+              healthcare settings. Registered Nurses assess patient health
+              needs, develop and implement nursing care plans, and maintain
+              medical records. They work closely with other healthcare
+              professionals to ensure the well-being and recovery of patients.
             </p>
             <a href="/contact">Apply Now</a>
           </div>
@@ -39,17 +53,6 @@ const Vacancies = () => {
             </p>
             <a href="/contact">Apply Now</a>
           </div>
-          <div className="card-11">
-            <h2>Apprentice Nurse</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
-              repellendus? Iusto debitis voluptatum velit impedit fuga,
-              inventore cum nesciunt officiis unde tempora quasi, assumenda
-              provident praesentium esse natus vero commodi.
-            </p>
-            <a href="/contact">Apply Now</a>
-          </div>
-          
         </div>
       </div>
     </Layout>
