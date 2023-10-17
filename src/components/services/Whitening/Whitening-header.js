823d62256d2d0beea3616f901c9711b33a92c933
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-17 15:19:02 +0600

    commit

--- src/components/services/Whitening/Whitening-header.js
+++ src/components/services/Whitening/Whitening-header.js
@@ -1,55 +1,46 @@
 import React from "react";
 const BlogPage = () => {
   return (
-      <section>
-        <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
-          <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
-            <svg
-              className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
-              viewBox="0 0 100 100"
-              fill="currentColor"
-              preserveAspectRatio="none slice"
-            >
-              <path d="M50 0H100L50 100H0L50 0Z" />
-            </svg>
-            <img
-              className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
-              src="https://res.cloudinary.com/db1i46uiv/image/upload/v1696596929/woman-smiling-while-male-dentist-keeping-teeth-color-range_wjzulv.jpg"
-              alt=""
-            />
-          </div>
-          <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
-            <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              
-              </p>
-              <h2 className="mb-5 blue text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                Designed by Dentists {" "}
-                <br className="hidden md:block" />
+    <section>
+      <div className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
+        <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+          <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+            <div className="max-w-xl mb-6">
+              <div>
+                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                  New Colaboration
+                </p>
+              </div>
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                Designed by Dentists <br className="hidden md:block" />
                 Developed for You
               </h2>
-              <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
+              <p className="text-base text-gray-700 md:text-lg">
                 We offer a carefully curated selection of whitening ranges
                 designed to effectively remove stains and discoloration, giving
                 you the confidence to flash your pearly whites.
               </p>
-              <div className="flex items-center">
-                
-                <a
-                  href="https://uk.dentalhub.online/v1/view/organization/6119/index.html#/perspectives/3"
-                  aria-label=""
-                  className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-                >
-                  Book An Appointment 
-                </a>
-              </div>
+            </div>
+            <div className="flex flex-col items-center md:flex-row">
+              <a
+                href="/"
+                className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              >
+                Book An Appointment
+              </a>
             </div>
           </div>
         </div>
-      </section>
+        <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+            src="https://res.cloudinary.com/db1i46uiv/image/upload/v1696596929/woman-smiling-while-male-dentist-keeping-teeth-color-range_wjzulv.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default BlogPage;
-
-
