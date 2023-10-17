77465e27af0a23a36bf319c19ae2d323e156efb6
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-17 22:00:02 +0600

    commut

---
+++ src/components/about/about-me.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const WhyChooseUs = () => {
+  return (
+    <section className=" container mx-auto px-10">
+      <div class="angry-grid">
+        <div id="item-0">&nbsp;</div>
+        <div id="item-1">&nbsp;</div>
+        <div id="item-2">&nbsp;</div>
+        <div id="item-3">&nbsp;</div>
+        <div id="item-4">&nbsp;</div>
+        <div id="item-5">&nbsp;</div>
+        <div id="item-6">&nbsp;</div>
+        <div id="item-7">&nbsp;</div>
+        <div id="item-8">&nbsp;</div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
