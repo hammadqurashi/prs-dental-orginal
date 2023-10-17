895ab4a61e3696c5b196e9bd1c08056214494356
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-17 22:39:09 +0600

    commit

--- src/components/about/about-me.jsx
+++ src/components/about/about-me.jsx
@@ -2,19 +2,11 @@ import React from "react";
 const WhyChooseUs = () => {
   return (
     <section className=" container mx-auto px-10">
-      <div className="angry-grid">
-        <div id="item-0">
-          <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1697559228/WhatsApp_Image_2023-10-17_at_22.10.36_d012e3c5_gx6ajz.jpg" alt="" />
+
+        <div>
+          <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1697560680/Picture1_bpvsvo.jpg" />
         </div>
-        <div id="item-1">&nbsp;</div>
-        <div id="item-2">&nbsp;</div>
-        <div id="item-3">&nbsp;</div>
-        <div id="item-4">&nbsp;</div>
-        <div id="item-5">&nbsp;</div>
-        <div id="item-6">&nbsp;</div>
-        <div id="item-7">&nbsp;</div>
-        <div id="item-8">&nbsp;</div>
-      </div>
+
     </section>
   );
 };
