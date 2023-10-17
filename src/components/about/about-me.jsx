3ac4d73aac82af653974d26e4559b8d0c59e0aa3
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-17 22:06:54 +0600

    commut

--- src/components/about/about-me.jsx
+++ src/components/about/about-me.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const WhyChooseUs = () => {
   return (
     <section className=" container mx-auto px-10">
-      <div class="angry-grid">
+      <div className="angry-grid">
         <div id="item-0">&nbsp;</div>
         <div id="item-1">&nbsp;</div>
         <div id="item-2">&nbsp;</div>
