28dc6627cc5b1c3ac6042f1bcc605a2c8c7336fc
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-17 22:14:29 +0600

    commiy

--- src/components/about/about-me.jsx
+++ src/components/about/about-me.jsx
@@ -3,7 +3,9 @@ const WhyChooseUs = () => {
   return (
     <section className=" container mx-auto px-10">
       <div className="angry-grid">
-        <div id="item-0">&nbsp;</div>
+        <div id="item-0">
+          <img src="https://res.cloudinary.com/db1i46uiv/image/upload/v1697559228/WhatsApp_Image_2023-10-17_at_22.10.36_d012e3c5_gx6ajz.jpg" alt="" />
+        </div>
         <div id="item-1">&nbsp;</div>
         <div id="item-2">&nbsp;</div>
         <div id="item-3">&nbsp;</div>
