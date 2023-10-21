83a88c83255d9b720e5284f177851b0276844960
zisam shaikh <zisamshaikh7@gmail.com>
2023-10-22 02:17:57 +0600

    commit

--- src/components/about/about-me.jsx
+++ src/components/about/about-me.jsx
@@ -3,10 +3,20 @@ const WhyChooseUs = () => {
   return (
     <section className=" container mx-auto px-10 mb-24">
       <div class="container-grid">
-        <div class="image"></div>
+        <div class="image">
+          <img
+            src="https://res.cloudinary.com/db1i46uiv/image/upload/v1697918420/WhatsApp_Image_2023-10-22_at_01.49.06_80aa571f_oaukua.jpg"
+            alt=""
+          />
+          <hr />
+          <p>
+            The Late Dr Pav Bhogal
+            <br /> 03/07/1992 to 16/08/2023 <br />
+            <b>Age 31</b>
+          </p>
+        </div>
+
         <div class="intro"></div>
-        <div class="desc"></div>
-        <div class="desc2"></div>
       </div>
     </section>
   );
