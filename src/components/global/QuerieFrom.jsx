7296dd9a5e50eab712d37280f77cb12b008bac67
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-01 01:36:06 +0600

    commut

--- src/components/global/QuerieFrom.jsx
+++ src/components/global/QuerieFrom.jsx
@@ -27,37 +27,41 @@ export const ContactUs = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        className="px-6 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
         placeholder="First Name"
         type="text"
         name="first_name"
       />
       <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        className="px-6 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
         placeholder="Last Name"
         type="text"
         name="last_name"
       />
       <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        className="px-6 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
         placeholder="Phone Number"
         type="number"
         name="number"
       />
       <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        className="px-6 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
         placeholder="Email"
         type="email"
         name="user_email"
       />
       <textarea
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        className="px-6 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
         placeholder="Message"
         name="message"
         rows={5}
       />
       <a href="#">
-        <input type="submit" value="Send" />
+        <input
+          className=" blue px-4  w-full py-4 flex justify-center items-center"
+          type="submit"
+          value="Send"
+        />
       </a>
     </form>
   );
