30dbfc6aeb65d4f98940f4adbc7a0c4400b6d30a
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-01 01:26:21 +0600

    commit

--- src/components/global/QuerieFrom.jsx
+++ src/components/global/QuerieFrom.jsx
@@ -26,11 +26,36 @@ export const ContactUs = () => {
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <input placeholder="First Name" type="text" name="first_name" />
-      <input placeholder="Last Name" type="text" name="last_name" />
-      <input placeholder="Phone Number" type="number" name="number" />
-      <input placeholder="Email" type="email" name="user_email" />
-      <textarea placeholder="Message" name="message" />
+      <input
+        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        placeholder="First Name"
+        type="text"
+        name="first_name"
+      />
+      <input
+        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        placeholder="Last Name"
+        type="text"
+        name="last_name"
+      />
+      <input
+        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        placeholder="Phone Number"
+        type="number"
+        name="number"
+      />
+      <input
+        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        placeholder="Email"
+        type="email"
+        name="user_email"
+      />
+      <textarea
+        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-2"
+        placeholder="Message"
+        name="message"
+        rows={5}
+      />
       <a href="#">
         <input type="submit" value="Send" />
       </a>
