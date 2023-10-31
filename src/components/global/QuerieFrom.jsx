d8b7c7fb2ca706a4e0574775297f8540fb798e32
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-01 02:48:24 +0600

    commit

--- src/components/global/QuerieFrom.jsx
+++ src/components/global/QuerieFrom.jsx
@@ -1,16 +1,17 @@
+
 import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
-import { NormalButton } from "./Button";
 import { useNavigate } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 
-export const ContactUs = () => {
+const ContactForm = () => {
   const form = useRef();
-
 
   const navigate = useNavigate();
   const sendEmail = (e) => {
     e.preventDefault();
-    
+    // redirecting to thank page
+    navigate("/thank-you");
+
     emailjs
       .sendForm(
         "service_fdpjl7j",
@@ -29,63 +30,44 @@ export const ContactUs = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <div
-            className={error ? "empty_notice" : "returnmessage"}
-            style={{ display: error == null ? "none" : "block" }}
-          >
-            <span>
-              {error
-                ? "Please Fill Required Fields"
-                : "We will contact you soon."}
-            </span>
-          </div>
-      <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
-        placeholder="First Name"
-        type="text"
-        name="first_name"
-      />
-      <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
-        placeholder="Last Name"
-        type="text"
-        name="last_name"
-      />
-      <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
-        placeholder="Phone Number"
-        type="number"
-        name="number"
-      />
-      <input
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
-        placeholder="Email"
-        type="email"
-        name="user_email"
-      />
-      <textarea
-        className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
-        placeholder="Message"
-        name="message"
-        rows={5}
-      />
-      <a href="#">
-        <input
-          className=" blc px-4  w-full py-4 flex justify-center items-center"
-          type="submit"
-          value="Send"
-        />
-        <NormalButton onClick={navigate("/")}
-          variant={"blue"}
-          className="px-3  w-full py-3 flex justify-center items-center"
-          type="submit"
-        >
-          Submit
-        </NormalButton>
-      </a>
-    </form>
+    <React.Fragment>
+      <h2>EmailJS</h2>
+      <form onSubmit={sendEmail} ref={form}>
+        <div className="fields">
+          <label>Full Name (required)</label>
+          <input type="text" name="from_name" required />
+        </div>
+        <div className="fields">
+          <label>Email (required)</label>
+          <input
+            type="email"
+            name="from_email"
+            required
+            placeholder="example@yourmail.com"
+          />
+        </div>
+        <div className="fields">
+          <label>Phone Number (required)</label>
+          <input type="number" name="phone" required placeholder="+123456789" />
+        </div>
+        <div className="fields">
+          <label>Subject (required)</label>
+          <input type="text" name="subject" required />
+        </div>
+        <div className="fields">
+          <label>Message (required)</label>
+          <textarea
+            name="message"
+            required
+            placeholder="Type your message here..."
+          />
+        </div>
+        <div className="fields">
+          <input type="submit" value="send" />
+        </div>
+      </form>
+    </React.Fragment>
   );
 };
 
-export default ContactUs;
+export default ContactForm;
