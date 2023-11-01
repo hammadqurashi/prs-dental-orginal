c96c62dc8b2af793591d32424582f1531d14917a
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-01 12:18:03 +0600

    commit

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import { NormalButton } from "./Button";
+import { NormalButton } from "../components/global/Button";
 
 export const ContactUs = () => {
   const form = useRef();
