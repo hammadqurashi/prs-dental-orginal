970e5fb225c911d7d70e5d832f8f3e4abf4dba10
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-12 01:33:29 +0600

    commit

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,111 +1,54 @@
-import emailjs from "emailjs-com";
-import { useState } from "react";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
 
-const Contact = () => {
-  const [mailData, setMailData] = useState({
-    name: "",
-    email: "",
-    topic: "",
-  });
-  const { name, email, topic } = mailData;
-  const [error, setError] = useState(null);
-  const onChange = (e) =>
-    setMailData({ ...mailData, [e.target.name]: e.target.value });
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (name.length === 0 || email.length === 0 || topic.length === 0) {
-      setError(true);
-      clearError();
-    } else {
-      emailjs
-        .send(
-          "service_7j2q9u5", // service id
-          "template_v07oovg", // template id
-          mailData,
-          "czNWW2UlwlqT2eYs-" // public api
-        )
-        .then(
-          (response) => {
-            setError(false);
-            clearError();
-            setMailData({ name: "", email: "", topic: "" });
-          },
-          (err) => {
-            console.log(err.text);
-          }
-        );
-    }
+
+const Sitemap = () => {
+  const {
+    register,
+    errors,
+    handleSubmit,
+    reset,
+    formState: { isSubmitted },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+
+    reset();
   };
-  const clearError = () => {
-    setTimeout(() => {
-      setError(null);
-    }, 2000);
-  };
+
   return (
-    <>
-      
-      <div className="frommzz ">
-        <form
-          action="/"
-          method="post"
-          className="contact_form"
-          id="contact_form"
-          autoComplete="off"
-          onSubmit={(e) => onSubmit(e)}
-        >
-          <div
-            className={error ? "empty_notice" : "returnmessage"}
-            style={{ display: error == null ? "none" : "block" }}
-          >
-            <span>
-              {error
-                ? "Please Fill Required Fields"
-                : "We will contact you soon."}
-            </span>
-          </div>
-          <div className="input_list">
-            <ul>
-              <li>
-                <input
-                  id="name"
-                  type="text"
-                  name="name"
-                  onChange={(e) => onChange(e)}
-                  value={name}
-                  placeholder="Your Name"
-                />
-              </li>
-              <li>
-                <input
-                  id="email"
-                  type="text"
-                  placeholder="Your Email"
-                  name="email"
-                  onChange={(e) => onChange(e)}
-                  value={email}
-                />
-              </li>
-              <li>
-                <input
-                  id="topic"
-                  type="text"
-                  placeholder="What Are You Looking For"
-                  name="topic"
-                  onChange={(e) => onChange(e)}
-                  value={topic}
-                />
-              </li>
-            </ul>
-          </div>
-          <div className="aali_tm_buttonzzz">
-            <a id="send_message" href="#" onClick={(e) => onSubmit(e)}>
-              <span>Book now </span>
-            </a>
-          </div>
-        </form>
-      </div>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Error</label>
+      <input
+        type="text"
+        name="singleErrorInput"
+        ref={register({ required: true })}
+      />
+      {errors.singleErrorInput && <p>Your input is required</p>}
+
+      <label>Error with type check</label>
+      <input
+        type="text"
+        name="multipleErrorInput"
+        ref={register({ required: true, maxLength: 50 })}
+      />
+      {errors.multipleErrorInput &&
+        errors.multipleErrorInput.type === "required" && (
+          <p>Your input is required</p>
+        )}
+      {errors.multipleErrorInput &&
+        errors.multipleErrorInput.type === "maxLength" && (
+          <p>Your input exceed maxLength</p>
+        )}
+
+      <label>Error with value</label>
+      <input type="number" name="numberInput" ref={register({ min: 50 })} />
+      {errors.numberInput && <p>Your input required to be more than 50</p>}
+
+      <input type="submit" />
+    </form>
   );
 };
 
-export default Contact;
+export default Sitemap;
