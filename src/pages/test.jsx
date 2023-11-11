4e5c021a338cc4d917ac8e7e9f99a488bf96d481
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-11 22:26:36 +0600

    commit

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,29 +1,54 @@
-import {useState} from 'react';
+// import {useState} from 'react';
 
-const App = () => {
-  // const [message, setMessage] = useState('');
+// const App = () => {
+//   const [message, setMessage] = useState('');
 
-  // const handleChange = event => {
-  //   setMessage(event.target.value);
-  // };
+//   const handleChange = event => {
+//     setMessage(event.target.value);
+//   };
 
-  // const handleClick = () => {
-  //   setMessage('');
-  // };
+//   const handleClick = () => {
+//     setMessage('');
+//   };
 
+//   return (
+// <div>
+//   <input
+//     id="message"
+//     name="message"
+//     type="text"
+//     onChange={handleChange}
+//     value={message}
+//   />
+
+//   <button onClick={handleClick}>Clear field</button>
+// </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import Layout from "../components/global/Layout";
+import Form from "../components/global/form";
+
+const AboutPage = () => {
   return (
-    <div>
-      <input
-        id="message"
-        name="message"
-        type="text"
-        onChange={handleChange}
-        value={message}
-      />
+    <Layout>
+      <div>
+        <input
+          id="message"
+          name="message"
+          type="text"
+          onChange={handleChange}
+          value={message}
+        />
 
-      <button >Clear field</button>
-    </div>
+        <button>Clear field</button>
+      </div>
+      <Form />
+    </Layout>
   );
 };
 
-export default App;
+export default AboutPage;
