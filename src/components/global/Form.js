14f5ba57ba0eb5aaee4157eea35b315ec52d015f
zisam shaikh <zisamshaikh7@gmail.com>
2023-11-14 19:42:23 +0600

    commit

--- src/components/global/Form.js
+++ src/components/global/Form.js
@@ -1,180 +1,127 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { NormalButton } from "./Button";
-import axios from "axios";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Loader from "./Loader";
+import { Spin } from "antd";
 
-const Error = ({ errors, name }) => {
-  return (
-    <ErrorMessage
-      errors={errors}
-      name={name}
-      render={({ message }) => (
-        <p className="text-red-600 text-sm">{message}</p>
-      )}
-    />
-  );
-};
+export const ContactUs = () => {
+  const [success, setsuccess] = useState(false);
+  const [Loading, setLoading] = useState(false);
+  const form = useRef();
+  const fname = useRef("");
+  const [
+    firstName,
+    setfirstName,
+    lastName,
+    setlastName,
+    Number,
+    setNumber,
+    Email,
+    setEmail,
+    Message,
+    setMessage,
+  ] = useState("");
 
-const Form = ({ inputBg, oneCol, isModal }) => {
-  const toastId = React.useRef(null);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setsuccess(true);
+    setLoading(true);
+    setTimeout(() => {
+      fname.current.value = "";
+    }, 1000);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
 
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const notify = () => {
-    if (!toast.isActive(toastId.current)) {
-      toastId.current = toast.success("Appointment request successful!", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        toastId: "customId",
-      });
-    }
+    emailjs
+      .sendForm(
+        "service_fdpjl7j",
+        "template_qwpl58r",
+        form.current,
+        "KOS94cM91AU01eTnJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
-  const url = `${process.env.SERVER_URL}/appointment`;
-
-  const onSubmit = (data) => {
-    setIsLoading(true);
-    axios({
-      method: "post",
-      url: url,
-      data: data,
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          notify();
-        }
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err.response.status === 404) {
-          setErrorMessage("Something went wrong. Try again later.");
-        }
-      });
-  };
-
-  const inputClasses = [
-    `${inputBg} px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-2 transition-all w-full text-gray-400 focus:text-gray-600 py-2`,
-  ];
-
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} id="myForm">
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <div
-          className={`grid ${oneCol ? "grid-cols-1" : "grid-cols-2"}  gap-3`}
-        >
-          <div>
-            <input
-              placeholder="First Name"
-              className={`${inputClasses}`}
-              {...register("firstName", { required: "First name is required" })}
-            />
-            <Error name="firstName" errors={errors} />
-          </div>
-
-          <div>
-            <input
-              placeholder="Last Name"
-              className={`${inputClasses}`}
-              {...register("lastName", { required: "Last name is required" })}
-            />
-            <Error name="lastName" errors={errors} />
-          </div>
-        </div>
-
-        <div
-          className={`grid ${oneCol ? "grid-cols-1" : "grid-cols-2"}  gap-3`}
-        >
-          <div>
-            <input
-              placeholder="Phone"
-              className={`${inputClasses}`}
-              {...register("phone", { required: "Phone number is required" })}
-            />
-            <Error name="phone" errors={errors} />
-          </div>
-
-          <div>
-            <input
-              placeholder="Email"
-              className={`${inputClasses}`}
-              {...register("email", {
-                pattern: {
-                  value:
-                    /^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9-]+)(\.[a-zA-Z]{2,5}){1,2}$/i,
-                  message: "Invalid email address",
-                },
-                required: "Email is required",
-              })}
-            />
-            <Error name="email" errors={errors} />
-          </div>
-        </div>
-
-        <div
-          className={`grid ${oneCol ? "grid-cols-1" : "grid-cols-2"}  gap-3`}
-        >
-          <div>
-            <input
-              type="text"
-              placeholder="Date of Birth - DD/MM/YYYY"
-              className={`${inputClasses}`}
-              {...register("dob")}
-            />
-
-            <Error name="dob" errors={errors} />
-          </div>
-
-          <div>
-            <input
-              placeholder="Your address..."
-              className={`${inputClasses}`}
-              {...register("address")}
-            />
-
-            <Error name="address" errors={errors} />
-          </div>
-        </div>
-
-        <textarea
-          placeholder="What type of appointment/ treatment do you feel you require?"
-          rows={5}
-          className={`${inputClasses}`}
-          {...register("message")}
+    <Spin spinning={Loading}>
+      <form ref={form} onSubmit={sendEmail}>
+        <input
+          className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-3 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
+          placeholder="First Name"
+          type="text"
+          required
+          name="first_name"
+          ref={fname}
+          value={firstName}
+          onChange={(e) => setfirstName(e.target.value)}
         />
-        <Error name="message" errors={errors} />
-
-        <p className="text-red-600 text-sm mb-4">{errorMessage}</p>
-
-        <NormalButton
-          disabled={isLoading ? true : false}
-          variant={"blue"}
-          className="px-3  w-full py-3 flex justify-center items-center"
-          type="submit"
-        >
-          {isLoading ? <Loader /> : null}
-          Submit
-        </NormalButton>
+        <input
+          className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-3 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
+          placeholder="Last Name"
+          type="text"
+          required
+          name="last_name"
+          ref={fname}
+          value={lastName}
+          onChange={(e) => setlastName(e.target.value)}
+        />
+        <input
+          minLength={10}
+          maxLength={11}
+          className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-3 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
+          placeholder="Phone Number"
+          type="number"
+          required
+          name="number"
+          ref={fname}
+          value={Number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <input
+          className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-3 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
+          placeholder="Email"
+          type="email"
+          required
+          name="user_email"
+          ref={fname}
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <textarea
+          minLength={200}
+          className="px-4 outline-none ring-2 ring-primary/10 focus:ring-2 focus:ring-primary/40 rounded  my-3 transition-all w-full text-neutral-400 focus:text-neutral-600 py-4"
+          placeholder="Message"
+          name="message"
+          required
+          rows={5}
+          ref={fname}
+          value={Message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <a href="#">
+          <NormalButton
+            variant={"blue"}
+            className="px-3  w-full py-4 flex justify-center items-center"
+            type="submit"
+          >
+            Submit
+          </NormalButton>
+        </a>
+        {success && (
+          <p className="text-center text-lg p-4 ">
+            Thanks for contacting us. We will be in touch with you shortly.
+          </p>
+        )}
       </form>
-    </>
+    </Spin>
   );
 };
 
-export default Form;
+export default ContactUs;
